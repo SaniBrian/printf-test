996240547bc9ec8f1b7ad0fd9abb29a2d9927465
Ayowale <haydexwisdom@gmail.com>
2023-04-25 09:09:54 +0000

    updated

--- rottestfile.c
+++
@@ -1,4 +0,0 @@
-ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n
-
-
-EBG13 ("ebgngr ol 13 cynprf", fbzrgvzrf ulcurangrq EBG-13) vf n fvzcyr yrggre fhofgvghgvba pvcure.
